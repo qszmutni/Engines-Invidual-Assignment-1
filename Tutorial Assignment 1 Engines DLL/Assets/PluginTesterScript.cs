15656740979d82d5f22d564ceb5db52c3156d32d
Quinn Szmutni <qszmutni@gmail.com>
2019-09-26 12:26:37 -0400

    Base unity project push

---
+++ Tutorial Assignment 1 Engines DLL/Assets/PluginTesterScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class PluginTesterScript : MonoBehaviour
+{
+    const string DLL_NAME = "Tutorial DLL";
+
+    [DllImport(DLL_NAME)]
+    private static extern int SimpleFunction();
+
+    [DllImport(DLL_NAME)]
+    private static extern int wrapperWrite();
+
+    [DllImport(DLL_NAME)]
+    private static extern void wrapperRead();
+
+    [DllImport(DLL_NAME)]
+    private static extern void wrapperDelete();
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) //Save
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+        }
+
+    }
+}
