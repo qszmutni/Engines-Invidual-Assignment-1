2ddcbaab6ad43f5f14d309329c9b1d5bd9598c49
Quinn Szmutni <qszmutni@gmail.com>
2019-09-26 14:57:08 -0400

    Working DLL
    Might polish later

--- Tutorial Assignment 1 Engines DLL/Assets/PluginTesterScript.cs
+++ Tutorial Assignment 1 Engines DLL/Assets/PluginTesterScript.cs
@@ -2,21 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
+
 public class PluginTesterScript : MonoBehaviour
 {
+
     const string DLL_NAME = "Tutorial DLL";
 
     [DllImport(DLL_NAME)]
     private static extern int SimpleFunction();
 
     [DllImport(DLL_NAME)]
-    private static extern int wrapperWrite();
+    private static extern int wrapperWrite(float x, float y, float z);
 
     [DllImport(DLL_NAME)]
     private static extern void wrapperRead();
 
     [DllImport(DLL_NAME)]
     private static extern void wrapperDelete();
+
+    [DllImport(DLL_NAME)]
+    private static extern string wrapperGet();
+
+    [DllImport(DLL_NAME)]
+    private static extern float wrapperX();
+
+    [DllImport(DLL_NAME)]
+    private static extern float wrapperY();
+
+    [DllImport(DLL_NAME)]
+    private static extern float wrapperZ();
+
+
 
 
 
@@ -26,11 +42,21 @@ public class PluginTesterScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q)) //Save
         {
-
+            wrapperWrite(transform.position.x, transform.position.y, transform.position.z);
         }
 
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)) //Load
         {
+            wrapperRead();
+
+            Vector3 newPos = new Vector3(wrapperX(), wrapperY(), wrapperZ());
+
+            transform.position = newPos;
+
+            Debug.Log(wrapperX());
+            Debug.Log(wrapperY());
+            Debug.Log(wrapperZ());
+
         }
 
     }
