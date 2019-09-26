d55bbca5a5e29761f90d21e3c0e26c2e31912d59
Quinn Szmutni <qszmutni@gmail.com>
2019-09-26 13:06:33 -0400

    Movement

---
+++ Tutorial Assignment 1 Engines DLL/Assets/ControllerHandel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ControllerHandel : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    public GameObject cube;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+        float translationX = Input.GetAxis("Vertical");
+        float translationY = Input.GetAxis("Horizontal");
+
+
+
+        transform.Translate(speed * translationY * Time.deltaTime, 0f, speed * translationX * Time.deltaTime);
+
+
+    }
+}
